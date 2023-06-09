c97e4fb0524701fe638914b35189dfe4f3e65e05
George Heeres <gheeres@gmail.com>
2023-06-08 23:20:01 -0500

    Initial project template / start.

---
+++ js/event-mouse.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict'
+  feather.replace({ 'aria-hidden': 'true' });
+
+  const events = [ 
+    'click', 
+    'mousedown', 
+    'mouseenter', 
+    'mouseleave', 
+    'mousemove', 
+    'mouseover', 
+    'mouseout', 
+    'mouseup', 
+  ];
+
+})();
---
+++ js/wisdom.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict'
+  feather.replace({ 'aria-hidden': 'true' });
+
+  const quotes = [
+    'Never let your best friends get lonely, keep disturbing them.',
+    'Sometimes I wish I was an octopus, so I could slap eight people at once.',
+    'If Cinderella’s shoe fit perfectly, then why did it fall off?',
+    'If you’re hotter than me, then that means I’m cooler than you.',
+    'My wallet is like an onion, opening it makes me cry.',
+    'My goal this weekend is to move, just enough so people don’t think I’m dead.',
+    'Lazy people fact #2347827309018287. You were too lazy to read that number.',
+    'Friends buy you food. Best friends eat your food.',
+    'Papercut: A tree’s final moment of revenge.',
+    'Common sense is like deodorant, those who need it the most never use it.',
+    'I don’t need a hair stylist, my pillow gives me a new hairstyle every morning.',
+    'Life always offers you a second chance. It’s called tomorrow.',
+    'My six pack is protected by a layer of fat.',
+    'When nothing is going right, go left.',
+  ];
+
+})();
