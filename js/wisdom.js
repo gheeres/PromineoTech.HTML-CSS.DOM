8bf399e27bf61d9c20e1ccddc2c144a8513fccb9
George Heeres <gheeres@gmail.com>
2023-08-15 16:00:57 -0500

    change: Working sample with group activity/solution

--- js/wisdom.js
+++ js/wisdom.js
@@ -3,15 +3,6 @@
   feather.replace({ 'aria-hidden': 'true' });
 
   /**
-   * Generates a random number.
-   * @param {Number} max The maximum value to include.
-   * @returns The random number
-   */
-  function random(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-  /**
    * Creates a random alert.
    * @returns {HTMLElement} An alert
    */
@@ -48,4 +39,15 @@
     const alert = createRandomAlert();
     wisdom.insertBefore(alert, wisdom.firstChild);
   });
+
+  // ----------------------------------------------------------------------
+
+  /**
+   * Generates a random number.
+   * @param {Number} max The maximum value to include.
+   * @returns The random number
+   */
+  function random(max) {
+    return Math.floor(Math.random() * max);
+  }  
 })();
