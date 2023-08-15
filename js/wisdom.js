8d11b102df3564744e643e21e83a6daa651b3bfa
George Heeres <gheeres@gmail.com>
2023-08-15 15:58:46 -0500

    change: integrate / include group sample / breakout

--- js/wisdom.js
+++ js/wisdom.js
@@ -19,4 +19,15 @@
     'When nothing is going right, go left.',
   ];
 
+
+  // ----------------------------------------------------------------------
+
+  /**
+   * Generates a random number.
+   * @param {Number} max The maximum value to include.
+   * @returns The random number
+   */
+  function random(max) {
+    return Math.floor(Math.random() * max);
+  }
 })();
